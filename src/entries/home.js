6f71d1078a0b9bd2e0bac55d7823c793c00fdf35
Alejandro Cortez <ziker@live.com.mx>
2018-10-07 15:34:09 -0500

    Add normalizr library, and normalized api.json data

--- src/entries/home.js
+++ src/entries/home.js
@@ -7,6 +7,7 @@ import Home from '../pages/containers/home';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer  from '../reducers/data';
+import normalizedData from '../schemas/index'
 
 const initialState = {
     data: {
@@ -14,7 +15,8 @@ const initialState = {
     },
     search:[]
 }
-
+console.dir(data)
+console.dir(normalizedData)
 const store = createStore(
     reducer,// reducer
     initialState, // initialState
